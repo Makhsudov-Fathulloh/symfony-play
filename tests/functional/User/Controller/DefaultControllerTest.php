3ccf3daa843b688e2f59195f765cea604d7f0de5
Makhsudov <mr.makhsudov@gmail.com>
2023-05-05 22:40:41 +0500

    update-services config

--- tests/functional/User/Controller/DefaultControllerTest.php
+++ tests/functional/User/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\User\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
